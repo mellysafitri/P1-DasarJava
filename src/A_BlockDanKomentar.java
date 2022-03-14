71aa01e0dbd6b97ab2b8c6672b73fd1734fac3cb
mellysafitri <mellysafitri056@gmail.com>
2022-03-14 15:13:59 +0700

    Initial project pertemuan 1 dasar java

---
+++ src/A_BlockDanKomentar.java
@@ -0,0 +1,15 @@
+public class A_BlockDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek dengan Java");
+        //contoh komentar 1 baris
+
+        /*
+        contoh komentar lebih dari 1 baris
+         */
+
+        /**
+         * Java Doc -->
+         */
+
+    }
+}
---
+++ src/SatuLagiBro.java
@@ -0,0 +1,8 @@
+public class SatuLagiBro {
+        public static void main(String[] args) {
+            System.out.println("ok");
+
+        }
+    }
+
+
